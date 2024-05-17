5399587aa2084247c0c5c1982db5fe0f9af3e382
Dali <mateuswy123@gmail.com>
2024-05-17 02:03:08 -0300

    fouth 1.1 commit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react'
 import LayoutAdmin from "@/components/LayoutAdmin";
-import NavLogado from '@/components/NavLogado';
+import Nav from '@/components/Nav';
 import { useSession } from "next-auth/react";
 
 
@@ -9,7 +9,7 @@ export default function Home() {
   const { status, data: session } = useSession();
   return (
     <LayoutAdmin>
-      <NavLogado/>
+      <Nav/>
       <main className="min-h-screen flex justify-center pt-6">
         <span>{`Meu DEUS ${session?.user?.email}`}</span>
       </main>
